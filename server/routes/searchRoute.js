8e93eaecf6aff47cbca29ddf0bc8f9b0b8cf480c
Zulfiqar Ali <8zali9@gmail.com>
2023-09-21 04:18:08 +0500

    Controller (searchRoute): 1. response modified, 2. logs handled.

--- server/routes/searchRoute.js
+++ server/routes/searchRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const url = require("url");
 const router = express.Router();
 const needle = require("needle");
-const logger = require("../logger");
+const logger = require("../utils/logger");
 const apicache = require("apicache");
 
 const API_URL = process.env.API_URL;
@@ -22,9 +22,17 @@ router.get("/", cache("2 minutes"), async (req, res) => {
     const response = await needle("get", `${API_URL}?${params}`);
     const responseData = response.body;
 
-    res.status(200).json(responseData); // this response has to be modified.
+    const formattedResults = responseData.items.map((item) => ({
+      title: item.title,
+      snippet: item.snippet,
+      link: item.link,
+    }));
 
-    logger.info(req.url.split("=")[1]);
+    res.status(200).json(formattedResults);
+
+    if ((process.env.NODE_ENV = "development")) {
+      logger.info(req.url.split("=")[1]);
+    }
   } catch (error) {
     res.status(500).json({ error });
   }
